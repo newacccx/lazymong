16c731c905e522eae1c5805c83c32c3cb14a9103
newacccx <119074899+newacccx@users.noreply.github.com>
2023-05-03 15:25:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Bindji/LazyPrincess-Mogenius.git /LazyPrincess-Mogenius
+  git clone https://github.com/newacccx/lazymong /lazymong
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess-Mogenius
+  git clone $UPSTREAM_REPO /lazymong
 fi
-cd /LazyPrincess-Mogenius
+cd /lazymong
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bye JK DEVOLOPER Bot...."
 python3 bot.py

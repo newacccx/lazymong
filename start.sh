0599a4557718a55fd6f005d1b06d6890fa101b62
newacccx <119074899+newacccx@users.noreply.github.com>
2023-05-03 15:27:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,23 @@
 if [ -z $UPSTREAM_REPO ]
+
 then
+
   echo "Cloning main Repository"
+
   git clone https://github.com/newacccx/lazymong /lazymong
+
 else
+
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
+
   git clone $UPSTREAM_REPO /lazymong
+
 fi
+
 cd /lazymong
+
 pip3 install -U -r requirements.txt
+
 echo "Starting Bye JK DEVOLOPER Bot...."
+
 python3 bot.py

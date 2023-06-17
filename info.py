f162041c6ef6c16126deb993a533a7d20c6d8b97
newacccx <119074899+newacccx@users.noreply.github.com>
2023-06-17 23:51:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_Files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001775402942'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '-1001534042135')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'HDCINEMA_1')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), False)
